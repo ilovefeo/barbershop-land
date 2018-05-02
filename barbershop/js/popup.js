6b61e762f6229e3d9706e32337f2fd3ca7bb2100
ilovefeo <ilovefeo@ukr.net>
2018-05-02 17:42:25 +0300

    Add files via upload

---
+++ barbershop/js/popup.js
@@ -0,0 +1,40 @@
+        var link = document.querySelector(".login-link");
+        var popup = document.querySelector(".modal-login");
+        var form = popup.querySelector("form");
+        var close = document.querySelector(".modal-close");
+        var login = popup.querySelector("[name=login]");
+        var password = popup.querySelector("[name=password]");
+        var storage = localStorage.getItem("login");
+
+        link.addEventListener("click", function(evt){
+            evt.preventDefault();
+            popup.classList.add("modal-show");
+            if (storage) {
+                login.value = storage;
+                password.focus();
+            } else {
+                login.focus();
+            }
+        });
+
+        close.addEventListener("click", function(evt){
+            evt.preventDefault();
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+        });
+
+        form.addEventListener("submit", function(evt){
+            if (!login.value || !password.value){
+            evt.preventDefault();
+            popup.classList.add("modal-error");
+            } else {
+                localStorage.setItem("login", login.value);
+            }
+        });
+        window.addEventListener("keydown", function(evt){
+            if (evt.keyCode === 27) {
+                if (popup.classList.contains("modal-show")) {
+                    popup.classList.remove("modal-show");
+                }
+            }
+        });
